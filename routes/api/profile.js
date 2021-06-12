166d2eb37c68adfe482cbcf5cf13abfa21b250e7
jayesh Borse <jayeshborse2405@gmail.com>
2021-06-12 14:19:22 +0530

    create & update profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,80 @@
 const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
-router.get("/", (request, response) => {
-  response.send("Profile Router");
-});
+const auth = require("../../middlewares/Auth");
+
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
+router.post(
+  "/",
+  auth,
+  [
+    body("status").notEmpty().withMessage("Status is Required"),
+    body("skills").notEmpty().withMessage("Skills is Required"),
+  ],
+  async (request, response) => {
+    let errors = validationResult(request);
+    if (!errors.isEmpty()) {
+      return response.status(400).json({ errors: errors.array() });
+    }
+    let {
+      company,
+      website,
+      location,
+      status,
+      skills,
+      bio,
+      githubusername,
+      youtube,
+      twitter,
+      facebook,
+      instagram,
+      linkdin,
+    } = request.body;
+
+    let profileFields = {};
+    profileFields.user = request.user.id;
+
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (status) profileFields.status = status;
+    if (skills) {
+      profileFields.skills = skills.split(",").map((skill) => skill.trim());
+    }
+    if (bio) profileFields.bio = bio;
+    if (githubusername) profileFields.githubusername = githubusername;
+
+    profileFields.social = {};
+
+    if (youtube) profileFields.social.youtube = youtube;
+    if (instagram) profileFields.social.instagram = instagram;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (linkdin) profileFields.social.linkdin = linkdin;
+
+    try {
+      let profile = await Profile.findOne({ user: request.user.id });
+
+      if (profile) {
+        profile = await Profile.findOneAndUpdate(
+          { user: request.user.id },
+          { $set: profileFields },
+          { new: true }
+        );
+        return response.status(200).json(profile);
+      }
+
+      profile = new Profile(profileFields);
+
+      response.status(200).json(profile);
+    } catch (error) {
+      console.error(error.msg);
+      response.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
